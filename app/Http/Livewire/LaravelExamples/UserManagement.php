4e5a5c568720522c2d482ff76800312a4a74ac58
Edizon P. Tabac <77231534+lokihanji@users.noreply.github.com>
2025-06-06 18:06:36 +0800

    upp

--- app/Http/Livewire/LaravelExamples/UserManagement.php
+++ app/Http/Livewire/LaravelExamples/UserManagement.php
@@ -6,6 +6,28 @@ use Livewire\Component;
 
 class UserManagement extends Component
 {
+    public $name = '';
+    public $email = '';
+    public $role = '';
+
+    protected $rules = [
+        'name' => 'required|min:3',
+        'email' => 'required|email',
+        'role' => 'required|in:admin,creator,member',
+    ];
+
+    public function save()
+    {
+        $this->validate();
+
+        // Here you would typically save the user to the database
+        // For now, we'll just reset the form since it's a demo
+        $this->reset(['name', 'email', 'role']);
+
+        // Close the modal using JavaScript
+        $this->dispatchBrowserEvent('close-modal');
+    }
+
     public function render()
     {
         return view('livewire.laravel-examples.user-management');
